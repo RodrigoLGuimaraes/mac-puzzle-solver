55d55a965017be8170d9f512bbeb1ab11ce5f58a
Rodrigo Guimaraes <rodrigo.guimaraes@pipefy.com>
2020-07-10 10:15:35 -0300

    Initial Commit

---
+++ puzzlesolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  puzzlesolver
+//
+//  Created by Rodrigo Guimaraes on 10/07/20.
+//  Copyright © 2020 RodrigoLG. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
